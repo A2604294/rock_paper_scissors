a63ebff01b5c01c755b98ca738b898ae8b488d5c
Aaron <magnifecent7@gmail.com>
2019-05-22 23:35:29 -0600

    Add buttons and display on webpage

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
+let choices = ["Rock", "Paper", "Scissors"];
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => addEventListener("click", game)); 
 
 function computerPlay() {
-    let choices = ["Rock", "Paper", "Scissors"];
     let randChoice = Math.floor(Math.random() * 3);
     return choices[randChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
-    let computerChoice = computerSelection.toLowerCase();
-    let playerChoice = playerSelection.toLowerCase();
 
-
-    if ((playerChoice == "rock" &&  computerChoice == "scissors") || (playerChoice == "scissors" &&  computerChoice == "paper") || (playerChoice == "paper" &&  computerChoice == "rock")) { 
+    if ((playerSelection == "Rock" &&  computerSelection == "Scissors") || (playerSelection == "Scissors" &&  computerSelection == "Paper") || (playerSelection == "Paper" &&  computerSelection == "Rock")) { 
         return "win"
 
-    } else if ((computerChoice == "rock" &&  playerChoice == "scissors") || (computerChoice == "scissors" &&  playerChoice == "paper") || (computerChoice == "paper" &&  playerChoice == "rock")) { 
+    } else if ((computerSelection == "Rock" &&  playerSelection == "Scissors") || (computerSelection == "Scissors" &&  playerSelection == "Paper") || (computerSelection == "Paper" &&  playerSelection == "Rock")) { 
         return "lose"
 
-    } else if (computerChoice == playerChoice) {
+    } else if (computerSelection == playerSelection) {
         return "tie"
 
     } else {
@@ -25,46 +25,53 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+let playerScore = 0, computerScore = 0;
 
-    for (var i = 0; i < 5; i++) {
-        const playerSelection = window.prompt("Choose a weapon: ");
-        const computerSelection = computerPlay();
+function game(e) {
 
-        //takes player input and makes the first letter capital and the rest lower.
-        const playerSelectionDisplay = playerSelection[0].toUpperCase() + playerSelection.substring(1, playerSelection.length + 1).toLocaleLowerCase();
+    const playerChoice = choices[e.target.id];
+    const computerChoice = computerPlay();
+    let result = playRound(playerChoice, computerChoice);
 
-        let result = playRound(playerSelection, computerSelection);
-        if (result == "win") {
-            console.log(`You Win! ${playerSelectionDisplay} beats ${computerSelection}`);
-            playerScore++;
+    displayRoundWinner(result, playerChoice, computerChoice);
+    displayRunningScore();
 
-        } else if (result == "lose") {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelectionDisplay}`);
-            computerScore++;
-
-        } else if (result == "tie") {
-            console.log(`Tie! Both choices were ${computerSelection}`);
-
-
-        } else {
-            console.log("Invalid input, you lose a point.");
-            computerScore++;
-        }
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`You won ${playerScore}:${computerScore}`);
-    
-    } else if (playerScore < computerScore) {
-        console.log(`You lost ${playerScore}:${computerScore}`);
-
-    } else {
-        console.log(`You tied ${playerScore}:${computerScore}`);
+    if (playerScore == 5 || computerScore == 5) {
+        displayFinalWinner();
     }
 }
 
-game();
+function displayRunningScore() {
+    const scoreDisplay = document.querySelector("#score-display");
+    scoreDisplay.textContent = `${playerScore}:${computerScore}`;
+}
 
+function displayRoundWinner(result, pChoice, cChoice) {
+    const winner = document.getElementById("winner-display");
+
+    if (result == "win") {
+        winner.textContent = `You Win! ${pChoice} beats ${cChoice}`;
+        playerScore++;
+
+    } else if (result == "lose") {
+        winner.textContent = `You Lose! ${cChoice} beats ${pChoice}`;
+        computerScore++;
+
+    } else if (result == "tie") {
+        winner.textContent = `Tie! Both choices were ${pChoice}`;
+    }
+}
+
+function displayFinalWinner() {
+    const finalWinner = document.querySelector("#final-winner-display");
+
+    if (playerScore > computerScore) {
+        finalWinner.textContent = "You win!"
+
+    } else if (computerScore > playerScore) {
+        finalWinner.textContent = "You win!"
+
+    } else {
+        finalWinner.textContent = "Tie"
+    }
+}
